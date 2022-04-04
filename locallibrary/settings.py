3c93c6484db1c7caf9ae81f2506da81a7230c3d5
karimali900 <90.karim@gmail.com>
2022-04-04 23:11:47 +0200

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -138,7 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_URL = '/staticfiles/'
+STATIC_ROOT = '/staticfiles/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
